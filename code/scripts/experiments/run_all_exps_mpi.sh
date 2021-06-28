27629a65635bc372a12a27ad60896b26786b4bda
Michael Beukman <michael.beukman@gmail.com>
2021-06-28 15:51:55 +0200

    Added command to the run scripts.

--- code/scripts/experiments/run_all_exps_mpi.sh
+++ code/scripts/experiments/run_all_exps_mpi.sh
@@ -1,22 +1,22 @@
 for proc in {1,2,4,8,14,28}; do
-    echo "PROC = $proc"
+    echo "PROC(s) = $proc"
     for rosen in {0,1}; do
         
         #annealing, exp 1 
-        mpirun -np $proc 1 1 $rosen
+        mpirun -np $proc ./bin/mpi 1 1 $rosen
         
         #annealing, exp 3,
-        mpirun -np $proc 1 3 $rosen
+        mpirun -np $proc ./bin/mpi 1 3 $rosen
 
         # annealing exp 4
-        mpirun -np $proc 1 4 $rosen
+        mpirun -np $proc ./bin/mpi 1 4 $rosen
     done
 
     for rosen in {0,1}; do
         #GA, exp 1, TSP and Rosen
-        mpirun -np $proc 0 1 $rosen
+        mpirun -np $proc ./bin/mpi 0 1 $rosen
 
         #GA, exp 2, TSP and Rosen.
-        mpirun -np $proc 0 2 $rosen
+        mpirun -np $proc ./bin/mpi 0 2 $rosen
     done
 done
--- code/scripts/experiments/run_all_exps_mpi_multiple_nodes.sh
+++ code/scripts/experiments/run_all_exps_mpi_multiple_nodes.sh
@@ -1,16 +1,16 @@
 for proc in {28, 56, 112, 224}; do
     #for proc in {224,}; do
-    echo "PROC = $proc"
+    echo "PROC(s) = $proc"
     for rosen in {0,1}; do
         
         #annealing, exp 1 
-        mpirun -np $proc 1 1 $rosen
+        mpirun -np $proc ./bin/mpi 1 1 $rosen
         
         #annealing, exp 3,
-        mpirun -np $proc 1 3 $rosen
+        mpirun -np $proc ./bin/mpi 1 3 $rosen
 
         # annealing exp 4
-        mpirun -np $proc 1 4 $rosen
+        mpirun -np $proc ./bin/mpi 1 4 $rosen
     done
 
     # Don't to multiple nodes GA, very slow.
